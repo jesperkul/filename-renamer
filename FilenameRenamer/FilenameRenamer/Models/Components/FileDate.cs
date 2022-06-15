e9e1e9204334bfb9e6705cf5efd1a4dedee81c86
Jesper Kullberg <dev@jesperkullberg.com>
2022-06-15 17:29:38 +0200

    Add ComboBox for picking last modified date format

--- FilenameRenamer/FilenameRenamer/Models/Components/FileDate.cs
+++ FilenameRenamer/FilenameRenamer/Models/Components/FileDate.cs
@@ -1,14 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using Avalonia.Controls;
 using FilenameRenamer.Models.Interfaces;
 
 namespace FilenameRenamer.Models.Components
 {
     public class FileDate : IComponentItem
     {
-        Avalonia.Controls.Button btn = new Avalonia.Controls.Button() { Content = "Last Modified Date" };
-        public Avalonia.Controls.Control Component { get => btn; set => throw new NotImplementedException(); }
 
-        public string GetStuff(FileInfo inputFile) => inputFile.LastWriteTime.ToShortDateString();
+        ComboBox cbx = new ComboBox() { Items = new List<ComboBoxItem>() { new ComboBoxItem() { Content = "YYYY-MM-DD" }, new ComboBoxItem() { Content = "HH-mm-ss" }  }, SelectedIndex = 0 };
+        public Control Component { get => cbx; set => throw new NotImplementedException(); }
+
+        public string GetStuff(FileInfo inputFile)
+        {
+            switch (cbx.SelectedIndex)
+            {
+                default:
+                case 0: return inputFile.LastWriteTime.ToShortDateString();
+                case 1: return inputFile.LastWriteTime.ToString("HH-mm-ss");
+            }
+        }
     }
 }
