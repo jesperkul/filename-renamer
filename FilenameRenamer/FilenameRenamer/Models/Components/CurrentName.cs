cc05946aa5125fc8b2216ce8b4942f0f1788958f
Jesper Kullberg <dev@jesperkullberg.com>
2022-06-15 11:01:29 +0200

    Temporarily remove find replace functionality

--- FilenameRenamer/FilenameRenamer/Models/Components/CurrentName.cs
+++ FilenameRenamer/FilenameRenamer/Models/Components/CurrentName.cs
@@ -7,35 +7,8 @@ namespace FilenameRenamer.Models.Components
     public class CurrentName : IComponentItem
     {
         public string Content { get => "Current Name"; set => throw new NotImplementedException(); }
-        
-        private bool findReplace = false;
 
-        private string textToFind = "";
-
-        private string textToReplaceWith = "";
-
-        public void UpdateFindReplace(bool findReplaceOn, string find, string replace)
-        {
-            findReplace = findReplaceOn;
-            textToFind = find;
-            textToReplaceWith = replace;
-        }
-
- 
-        public string GetStuff(FileInfo inputFile)
-        {
-            string name = Path.GetFileNameWithoutExtension(inputFile.Name);
-            if (findReplace)
-            {
-                if (textToFind.Contains("$lmd$"))
-                {
-                    textToFind = textToFind.Replace("$lmd$", inputFile.LastWriteTime.ToShortDateString());
-                } 
-                
-                name = name.Replace(textToFind, textToReplaceWith);
-            }
-
-            return name;
-        }
+        public string GetStuff(FileInfo inputFile) => Path.GetFileNameWithoutExtension(inputFile.Name);
+       
     }
 }
--- FilenameRenamer/FilenameRenamer/ViewModels/MainWindowViewModel.cs
+++ FilenameRenamer/FilenameRenamer/ViewModels/MainWindowViewModel.cs
@@ -60,10 +60,6 @@ namespace FilenameRenamer.ViewModels
             }
         }
 
-        public string? _textToFind { get; set; }
-        public string? _textToReplaceWith { get; set; }
-
-
         private object? _selectedObject;
         public object? SelectedObject
         {
@@ -108,24 +104,11 @@ namespace FilenameRenamer.ViewModels
 
         public void ApplyButtonClick() => Task.Run(async () =>
         {
-            UpdateFindAndReplace();
             CurrentlyWorking = true;
             await renameService.ExecuteRename(ComponentItems, fileHandler.DirectoryItems);
             CurrentlyWorking = false;
             // Update names in list after rename or discard?
         });
-
-        private void UpdateFindAndReplace()
-        {
-                foreach(var item in ComponentItems)
-                {
-                    var nameItem = item as CurrentName;
-                    if (nameItem != null)
-                    {
-                        nameItem.UpdateFindReplace(FindAndReplaceOn, _textToFind, _textToReplaceWith);
-                    }
-                }
-        }
 
         public void MoveComponent(IComponentItem component)
         {
